19f28f83c9464e58774a31d0352f08cb32980731
Kac Denys <kacdenis52@gmail.com>
2020-07-23 19:48:33 +0300

    first commit

---
+++ Kats_D_HW1/js/index.js
@@ -0,0 +1,86 @@
+/* Задание 1 */
+console.log("My first debug");
+
+/* Задание 2 */
+let age = 22;
+console.log(age);
+
+let myKat = "Hrystik";
+console.log(myKat);
+
+/* Задание 3 */
+age = 999;
+console.log(age);
+
+/* Задание 4 */
+let numb = 50;
+console.log(typeof numb);
+
+let stri = "ctoto tam";
+console.log(typeof stri);
+
+let admin = true;
+console.log(typeof admin);
+
+let nol;
+console.log(typeof plot);
+
+/* Задание 5 */
+const student = {
+  name: "Denys",
+  surname: "Kats",
+  isActive: false,
+};
+let name = "Denys";
+let surname = "Kats";
+let isActive = false;
+console.log(typeof isActive);
+
+/* Задание 6 */
+let fruits = ["apple", "orange", "lemon", "watermelon", "melon"];
+console.log(fruits);
+
+let numbers = [1, 2, 3, 4, 5];
+console.log(numbers);
+
+const stude1 = {
+  name: "Denyss",
+  age: 22,
+  isAdmin: true,
+};
+const stude2 = {
+  name: "Igor",
+  age: 76,
+  isAdmin: false,
+};
+const stude3 = {
+  name: "Kate",
+  age: 32,
+  isAdmin: false,
+};
+const stude4 = {
+  name: "Danil",
+  age: 56,
+  isAdmin: false,
+};
+const stude5 = {
+  name: "Arina",
+  age: 56,
+  isAdmin: false,
+};
+
+let names = [stude1, stude2, stude3, stude4, stude5];
+console.log(names);
+
+/* Задание *1 */
+
+let arr = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+var first = arr[0];
+console.log(first);
+var last = arr[arr.length - 1];
+console.log(last);
+
+/* Задание *2 */
+
+let greeting = `студент:, ${name} ${surname},сейчас активен: ${isActive}`;
+console.log(greeting);
